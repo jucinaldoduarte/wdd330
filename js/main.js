68a141219b16c195376e8c3286a9bff8495c349d
Jucinaldo Duarte <82725921+jucinaldoduarte@users.noreply.github.com>
2022-01-02 17:22:58 -0300

    Uploading files

--- js/main.js
+++ js/main.js
@@ -14,8 +14,7 @@ fetch(requestURL)
         
         let list = document.getElementById("list"); 
         
-        let li = document.createElement("li");
-        
+        let li = document.createElement("li");        
         
         list.appendChild(li); 
         
