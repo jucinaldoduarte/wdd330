77cfb32365725826842531390956dc5b0477e1a8
Jucinaldo Duarte <82725921+jucinaldoduarte@users.noreply.github.com>
2022-01-26 09:14:20 -0300

    Week 04: Adding notes

--- js/week4.js
+++ js/week4.js
@@ -1,12 +1,9 @@
 const form = document.forms['search'];
 const input = form.elements.searchInput;
 
-
-
-
 input.addEventListener('focus', () => input.style.backgroundColor = "grey");
 input.addEventListener('blur', () => input.style.backgroundColor = "white");
-input.addEventListener('change', () => alert('changed'), false);
+//input.addEventListener('change', () => alert('changed'), false);
 
 
 
