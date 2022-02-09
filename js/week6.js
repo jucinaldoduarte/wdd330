64a20a10566fe180bc7c9007e04fc917d34c76fb
Jucinaldo Duarte <82725921+jucinaldoduarte@users.noreply.github.com>
2022-02-09 08:12:32 -0300

    Working on Todo challenge

---
+++ js/week6.js
@@ -0,0 +1,90 @@
+//https://medium.com/walternascimentobarroso-pt/criando-um-todo-com-javascript-8622331367eb
+
+let i = 1;
+
+function addTask() {  
+  let textInput = document.getElementById("textInput")
+
+  //Create Div
+  let taskdiv = document.createElement('div');
+  taskdiv.setAttribute('id',`taskdiv${i}`);
+  taskdiv.setAttribute('class','taskdiv');
+  document.getElementById('holder').appendChild(taskdiv);
+  taskdiv.style.border = "thick solid #FFFFFF";
+
+  //Create checkbox
+  let checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.setAttribute('id',`checkbox${i}`);
+  checkbox.setAttribute('class','checkbox');
+  //document.getElementById('holder').appendChild(checkbox);  
+  document.getElementById(`taskdiv${i}`).appendChild(checkbox);  
+
+  //Create span
+  let task = document.createElement('span');
+  task.setAttribute('id',`task${i}`);
+  task.setAttribute('class', 'task');
+  //document.getElementById('holder').appendChild(task);
+  document.getElementById(`taskdiv${i}`).appendChild(task);
+
+  //Create button
+  let btnClose = document.createElement('button');
+  btnClose.setAttribute('id',`btnClose${i}`); 
+  btnClose.setAttribute('class', 'btnClose');   
+  document.getElementById(`taskdiv${i}`).appendChild(btnClose);
+  btnClose.textContent = "X";
+  btnClose.style.fontSize = "2rem";
+  btnClose.style.paddingTop = "0.5rem";
+  btnClose.style.paddingBottom = "0.5rem";
+  btnClose.style.background = "none";
+  btnClose.style.border = "none";
+  btnClose.style.color = "white";
+  btnClose.onclick = function(){    
+    this.parentElement.style.backgroundColor = "red";    
+    let taskToclose = document.getElementById(btnClose.parentNode.id);
+    taskToclose.style.display = "none";
+  }  
+
+  //Add input text to task
+  task.textContent = textInput.value;
+
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {      
+      task.style.textDecoration = "line-through";
+    } else {
+      task.style.textDecoration = "none";
+    }
+  }); 
+  
+
+  i = i + 1;
+  
+
+
+}
+
+function showAll(){
+  let Alltasks = document.getElementsByClassName("taskdiv"); 
+  Alltasks.style.display = block;
+}
+
+function showActive(){
+
+}
+
+function showComplete(){
+  
+}
+
+
+
+
+
+
+
+  
+
+ 
+  
+
+
