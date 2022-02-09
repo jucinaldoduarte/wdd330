001cf3cd1106674f93ca4ffa1ae46d6273e66f6d
Jucinaldo Duarte <82725921+jucinaldoduarte@users.noreply.github.com>
2022-02-09 08:04:48 -0300

    Adding To do list

--- js/week5.js
+++ js/week5.js
@@ -1,37 +1,16 @@
 //https://medium.com/walternascimentobarroso-pt/criando-um-todo-com-javascript-8622331367eb
 
 let i = 1;
-let count = 1;
-
-//let taskstotal = 0;
 
 function addTask() {  
   let textInput = document.getElementById("textInput")
 
-  if (i == 1){
-    //Create Div
-    let summary = document.createElement('div');
-    summary.setAttribute('id',`summary`);  
-    document.getElementById('dashboard').appendChild(summary); 
-    summary.style.border = "thick solid #FFFFFF";
-  
-    let tasksLeft = document.createElement('span');
-    tasksLeft.setAttribute('id',`tasksLeft`);
-    summary.appendChild(tasksLeft);
-    tasksLeft.textContent = `${countTasks()} tasks left`; 
-    
-  }   
-   //Create Div
+  //Create Div
   let taskdiv = document.createElement('div');
   taskdiv.setAttribute('id',`taskdiv${i}`);
   taskdiv.setAttribute('class','taskdiv');
   document.getElementById('holder').appendChild(taskdiv);
   taskdiv.style.border = "thick solid #FFFFFF";
-
-  tasksLeft.textContent = `${countTasks()} tasks left`; 
-
-  //taskstotal = taskstotal + 1;
-  
 
   //Create checkbox
   let checkbox = document.createElement('input');
@@ -64,9 +43,6 @@ function addTask() {
     this.parentElement.style.backgroundColor = "red";    
     let taskToclose = document.getElementById(btnClose.parentNode.id);
     taskToclose.style.display = "none";
-    //taskstotal = taskstotal - 1;
-    //tasksLeft.textContent = `${taskstotal} tasks left`;  
-    
   }  
 
   //Add input text to task
@@ -75,52 +51,30 @@ function addTask() {
   checkbox.addEventListener('change', function() {
     if (this.checked) {      
       task.style.textDecoration = "line-through";
-      //taskstotal = taskstotal - 1;
-      //tasksLeft.textContent = `${taskstotal} tasks left`;  
-      tasksLeft.textContent = `${countTasks()} tasks left`; 
-      
-    }     
-    else {
+    } else {
       task.style.textDecoration = "none";
-      //taskstotal = taskstotal + 1;
-      //tasksLeft.textContent = `${taskstotal} tasks left`; 
-      tasksLeft.textContent = `${countTasks()} tasks left`; 
-      
     }
-    
   }); 
-
-  
   
 
-
-  //tasksLeft.textContent = `${taskstotal} tasks left`;  
   i = i + 1;
-
-  //let count = document.getElementsByTagName('input').length;
   
+
 
 }
 
-function countTasks(){
-  count = document.getElementsByClassName('taskdiv').length;
-  return count;
+function showAll(){
+  let Alltasks = document.getElementsByClassName("taskdiv"); 
+  Alltasks.style.display = block;
 }
 
+function showActive(){
 
+}
 
-
-
-
-
-
-
-
-
-
-
+function showComplete(){
   
-
+}
 
 
 
