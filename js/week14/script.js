5d067ce5a15bfcf4dca7f533b59b8b4da60a09b2
Jucinaldo Duarte <82725921+jucinaldoduarte@users.noreply.github.com>
2022-03-16 14:20:07 -0300

    Final Project: Improvement javascript code

--- js/week14/script.js
+++ js/week14/script.js
@@ -1,26 +1,22 @@
-const appID = "4c4f7720e4f35419cea59c4ff154beb5"; 
-
+const appID = "4c4f7720e4f35419cea59c4ff154beb5";
 let currentWeatherDiv = document.getElementById("currentWeatherDiv");
 let forecastDiv = document.getElementById("forecastDiv");
+let alertFieldset = document.getElementById("alertFieldset");
 let alertDiv = document.getElementById("alertDiv");
-
 let local = document.createElement("SPAN");
 let alertEvent = document.createElement("SPAN");
 let alertDesc = document.createElement("SPAN");
 let alertSource = document.createElement("SPAN");
-
 let iconSun = "&#9728;";
 let iconCloud = "&#9729;";
 let iconUmbrella = "&#9730;";
 let iconSnowMan = "&#9731;";
-
 let iconArray = [];
-
+let iconAdd = "&#10010;";
 iconArray.push(iconSun);
 iconArray.push(iconCloud);
 iconArray.push(iconUmbrella);
 iconArray.push(iconSnowMan);
-
 let point = 0;
 
 function refreshPage() {
@@ -69,7 +65,6 @@ function formatString(text){
         textCapitalized = textCapitalized.join(" ");
         return textCapitalized;
 }
-
 (function(window){
 	window.htmlentities = {		
 		encode : function(str) {
@@ -111,8 +106,8 @@ function getWeatherData(position) {
           let icon = document.createElement("img");
           icon.src = iconURL;
           currentWeatherDiv.appendChild(icon);
-        }
-        
+        }  
+
         //Temperature
         if (jsObject.main.temp != 'undefined'){
           let temp = document.createElement("SPAN");
@@ -125,35 +120,47 @@ function getWeatherData(position) {
           let weatherDesc = document.createElement("SPAN");
           weatherDesc.textContent = formatString(jsObject.weather[0].description);
           currentWeatherDiv.appendChild(weatherDesc);
-        }        
+        }
+
+        //Details
+        let detailsDiv = document.createElement("DIV");
+        currentWeatherDiv.appendChild(detailsDiv);
+        detailsDiv.setAttribute("id", "detailsDiv");  
+        
+        let details = document.createElement('SPAN');
+        details.setAttribute("id", "detailsBtn");
+        details.setAttribute("onclick", "showDetails()")
+        let img = htmlentities.decode(iconAdd);
+        details.textContent = img;
+        currentWeatherDiv.appendChild(details);
 
         //Humidity
         if (jsObject.main.humidity != 'undefined'){
           let humidity = document.createElement("SPAN");
           humidity.textContent = `Humidity: ${jsObject.main.humidity}%`;
-          currentWeatherDiv.appendChild(humidity);
-        }      
+          detailsDiv.appendChild(humidity);
+        }
 
         //Wind Speed
         if (jsObject.wind.speed != 'undefined'){
           let windSpeed = document.createElement("SPAN");
           windSpeed.textContent = `Wind Speed: ${jsObject.wind.speed} mph`;
-          currentWeatherDiv.appendChild(windSpeed);
-        }        
+          detailsDiv.appendChild(windSpeed);
+        }
 
         //Wind Chill
         if(jsObject.main.temp <= 50 && jsObject.wind.speed > 3) {
           let windChill = document.createElement("SPAN");
           windChill.textContent = 'Wind Chill: ' + Math.round(35.74 + (0.6215 * jsObject.main.temp) - (35.75 * Math.pow(jsObject.wind.speed, 0.16)) +(0.4275 * jsObject.main.temp * Math.pow(jsObject.wind.speed, 0.16))) + ' ºF';
-          currentWeatherDiv.appendChild(windChill);
-        }   
+          detailsDiv.appendChild(windChill);
+        } 
     });
+
     //Forecast and alerts
     fetch(apiOneCall)
     .then((response) => response.json())
     .then((jsObject) => {  
         console.log(jsObject);
-
         //Forecast
         for (let i = 1; i < jsObject.daily.length; i++ )
         { 
@@ -182,29 +189,52 @@ function getWeatherData(position) {
         }
 
         //Alerts
-        for (let i = 0; i < jsObject.alerts.length; i++ )
-        { 
-            //Alert event 
-            alertEvent.textContent = `${jsObject.alerts[i].event}`;
-            alertEvent.style.color = "orange";
-            alertDiv.appendChild(alertEvent);              
+        if (jsObject.alerts){
+          alertFieldset.style.display = "block";
+          
+          for (let i = 0; i < jsObject.alerts.length; i++ )
+          { 
+              //Alert event              
+              alertEvent.textContent = `${jsObject.alerts[i].event}`;
+              alertEvent.style.color = "orange";
+              alertDiv.appendChild(alertEvent);
 
-            //Alert description
-            alertDesc.textContent = `${jsObject.alerts[i].description}`;
-            alertDesc.style.color = "white";
-            alertDiv.appendChild(alertDesc);  
+              //Alert description
+              alertDesc.textContent = `${jsObject.alerts[i].description}`;
+              alertDesc.style.color = "white";
+              alertDiv.appendChild(alertDesc); 
 
-            //Alert source
-            alertSource.textContent = `Source: ${jsObject.alerts[i].sender_name}`;
-            alertSource.style.color = "orange";
-            alertDiv.appendChild(alertSource);  
+              //Alert source
+              alertSource.textContent = `Source: ${jsObject.alerts[i].sender_name}`;
+              alertSource.style.color = "orange";
+              alertDiv.appendChild(alertSource);
 
-            //Line break
-            let br = document.createElement("BR");
-            alertDiv.appendChild(br);
-        }
+              //Line break
+              let br = document.createElement("BR");
+              alertDiv.appendChild(br);
+          }          
+        }        
     });
 }   
+
+function showDetails(){  
+  details = document.getElementById("detailsBtn");  
+  if(iconAdd=="&#9866;"){    
+    detailsDiv.style.display = "none";
+    
+    iconAdd = "&#10010;";
+    let img = htmlentities.decode(iconAdd);
+    details.textContent = img; 
+  }
+  else if(iconAdd=="&#10010;"){    
+    detailsDiv.style.display = "block";
+
+    iconAdd = "&#9866;";  
+    let img = htmlentities.decode(iconAdd);
+    details.textContent = img;
+  }  
+}
+
 getLocation();
 setInterval(changeIcon, 2500);
 changeIcon();
